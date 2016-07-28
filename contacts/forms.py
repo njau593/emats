176040efd9c328fae66bd6d6278dfdbc979375a6
Godfrey Njau <godfrey.njau@strathmore.edu>
2016-07-28 10:11:31 +0300

    contact form

--- contacts/forms.py
+++ contacts/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from .models import newsletter
 
-'''class newsletter_form(forms.ModelForm):
+class newsletter_form(forms.ModelForm):
     class Meta:
         model=newsletter
-        fields=['Name', 'Email',]'''
+        fields=['Name', 'Email',]
 
-class news_form(forms.Form):
+'''class news_form(forms.Form):
     sub_name=forms.CharField(max_length=100, required=False)
-    sub_email=forms.EmailField(required=True)
+    sub_email=forms.EmailField(required=True)'''
--- contacts/views.py
+++ contacts/views.py
@@ -1,49 +1,31 @@
 from django.shortcuts import render
 from .models import reachUs
-from .forms import news_form
-from django.template.loader import get_template
-from django.core.mail import EmailMessage
-from django.template import context
+from .forms import newsletter_form
 
 # Create your views here.
 
 def contacts(request):
     card=reachUs.objects.all()
-    '''form=newsletter_form(request.POST)
-    if form.is_valid():
-        post=form.save()
-        post.save()
-    else:
-        form=newsletter_form()'''
-
-    form_class=news_form
-    if request.method == 'POST':
-        form=form_class(data=request.POST)
+    #context=RequestContext(request)
+    if request.method=='POST':
+        form=newsletter_form(request.POST)
 
         if form.is_valid():
-            sub_name=request.POST.get('sub_name', '')
-            sub_email=request.POST.get('sub_email', '')
-            form_content=request.POST.get('content', '')
+            form.save(commit=True)
+            #return contacts(request)
+        else:
+            print (form.errors)
 
-            #Email the profile with the contact information
-            template=get_template('contact_template.txt')
-            example=context({
-                'sub_name':sub_name,
-                'sub_email':sub_email,
-                #'form_content':form_content,
-            })
-            content=template.render(example)
-
-            email=EmailMessage(
-                "New contact form submission",
-                content,
-                "Your website"+'',
-                ['njaugodfrey@outlook.com'],
-                headers={ 'Reply-To':sub_email }
-            )
-            email.send()
-            return redirect('contacts')
+    else:
+        form=newsletter_form()
 
     return render(request, 'pages/contacts.html', {
-    'card':card, 'form':form_class
+    'card':card, 'form':form
     })
+
+'''form=newsletter_form(request.POST)
+if form.is_valid():
+    post=form.save()
+    post.save()
+else:
+    form=newsletter_form()'''
